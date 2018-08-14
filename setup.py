35de8fbf4344359c54a5f12d46386b70bcea4468
Peter Caisse <pcaisse@gmail.com>
2018-08-14 12:41:07 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ tests_require = []
 
 setup(
     name='ashlar',
-    version='0.0.2',
+    version='0.0.3',
     description='Define and validate schemas for metadata for geotemporal event records',
     author='Azavea, Inc.',
     author_email='info@azavea.com',
